f133c816f673a07c0e56eb839590f7f6b2127ff4
Kristofer <kristofer@fmckl.se>
2024-04-23 00:06:37 +0200

    UI refactor

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -4,18 +4,19 @@ import RecipeSelectedView from "./views/recipe-selected/RecipeSelectedView";
 import useRunContext from "./ run-context/useRunContext";
 import LoginSection from "./components/login/LoginSection";
 import { useSiweIdentity } from "ic-use-siwe-identity";
+import GitHubIcon from "./components/GitHubIcon";
 
 function App() {
   const { selectedRecipe } = useRunContext();
   const { identity } = useSiweIdentity();
 
   return (
-    <div className="flex flex-col items-center w-full">
+    <div className="flex flex-col items-center w-full pb-10">
       <Header />
       <div className="flex flex-col items-center gap-10 p-5">
-        <div className="h-5 md:h-10" />
         {!identity && <LoginSection />}
         {selectedRecipe ? <RecipeSelectedView /> : <MainView />}
+        <GitHubIcon />
       </div>
     </div>
   );
--- src/frontend/src/components/GitHubIcon.tsx
+++ src/frontend/src/components/GitHubIcon.tsx
@@ -4,11 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function GitHubIcon() {
   return (
     <div className="flex flex-col items-center w-full max-w-2xl gap-5 text-center">
-      <a
-        href="https://github.com/kristoferlund/ic-siwe-react-demo-rust"
-        rel="noreferrer"
-        target="_blank"
-      >
+      <a href="https://github.com/c-atts" rel="noreferrer" target="_blank">
         <FontAwesomeIcon
           className="w-10 h-10 mx-3 text-zinc-500"
           icon={faGithub}
