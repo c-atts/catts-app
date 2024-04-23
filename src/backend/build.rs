b1c9ecbd88b1efae5f5d82f414501ee20d733807
Kristofer <kristofer@fmckl.se>
2024-04-23 12:25:22 +0200

    remove reliance on .env

--- src/backend/build.rs
+++ src/backend/build.rs
@@ -1,14 +1,26 @@
 use dotenv::dotenv;
 use ic_cdk_bindgen::{Builder, Config};
+use std::env;
 use std::path::PathBuf;
 
-/// This build script generates bindings in the declarations module to simplify interacting
-/// with the deployed ic_siwe_provider canister.
 fn main() {
     dotenv().ok();
 
     let manifest_dir =
-        PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").expect("Cannot find manifest dir"));
+        PathBuf::from(env::var("CARGO_MANIFEST_DIR").expect("Cannot find manifest dir"));
+
+    let ic_siwe_provider_did_path = manifest_dir.join("../ic_siwe_provider/ic_siwe_provider.did");
+    let evm_rpc_did_path = manifest_dir.join("../evm_rpc/evm_rpc.did");
+
+    let ic_siwe_provider_did_str = ic_siwe_provider_did_path.to_str().expect("Path invalid");
+    let evm_rpc_did_str = evm_rpc_did_path.to_str().expect("Path invalid");
+
+    env::set_var(
+        "CANISTER_CANDID_PATH_IC_SIWE_PROVIDER",
+        ic_siwe_provider_did_str,
+    );
+    env::set_var("CANISTER_CANDID_PATH_EVM_RPC", evm_rpc_did_str);
+
     let mut builder = Builder::new();
 
     // ic_siwe_provider
