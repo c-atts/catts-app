fcb20eae58e3ebcf919b858c10665d80e2724ede
Kristofer <kristofer@fmckl.se>
2024-05-09 16:45:22 +0200

    Fix footer

--- packages/catts_frontend/src/components/GitHubIcon.tsx
+++ packages/catts_frontend/src/components/GitHubIcon.tsx
@@ -1,13 +1,24 @@
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faGithub,
+  faTwitter,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function GitHubIcon() {
   return (
-    <div className="flex flex-col items-center w-full max-w-2xl gap-5 text-center">
+    <div className="flex flex-row items-center justify-center w-full gap-5 pt-10">
       <a href="https://github.com/c-atts" rel="noreferrer" target="_blank">
         <FontAwesomeIcon
           className="w-10 h-10 mx-3 text-zinc-500"
           icon={faGithub}
+        />
+      </a>
+      <a href="https://twitter.com/c_atts" rel="noreferrer" target="_blank">
+        <FontAwesomeIcon
+          className="w-10 h-10 mx-3 text-zinc-500"
+          icon={faXTwitter}
         />
       </a>
     </div>
--- packages/catts_frontend/src/routes/__root.tsx
+++ packages/catts_frontend/src/routes/__root.tsx
@@ -1,13 +1,15 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import Header from "../components/header/Header";
+import GitHubIcon from "../components/GitHubIcon";
 
 export const Route = createRootRoute({
   component: () => (
     <div className="flex flex-col items-center w-full pb-10">
       <Header />
       <Outlet />
-      <TanStackRouterDevtools />
+      <GitHubIcon />
+      {import.meta.env.DEV && <TanStackRouterDevtools />}
     </div>
   ),
 });
