583f7d01347c0c2a76dc9bb81572e50cf0668192
Kristofer <kristofer@fmckl.se>
2024-07-06 21:00:13 +0200

    Use SIWE_PROVIDER_CANISTER_ID when looking up user eth address

--- packages/catts_engine/src/siwe.rs
+++ packages/catts_engine/src/siwe.rs
@@ -2,9 +2,10 @@ use candid::Principal;
 use serde_bytes::ByteBuf;
 
 use crate::{
-    declarations::ic_siwe_provider::{ic_siwe_provider, GetAddressResponse},
+    declarations::ic_siwe_provider::{GetAddressResponse, IcSiweProvider},
     error::Error,
     eth::EthAddress,
+    SIWE_PROVIDER_CANISTER_ID,
 };
 
 pub async fn get_authenticated_eth_address() -> Result<EthAddress, Error> {
@@ -15,10 +16,22 @@ pub async fn get_authenticated_eth_address() -> Result<EthAddress, Error> {
         ));
     }
 
-    let response = ic_siwe_provider
-        .get_address(ByteBuf::from(caller.as_slice()))
-        .await
-        .map_err(|e| Error::internal_server_error(format!("Code: {:?}, message: {}", e.0, e.1)))?;
+    let maybe_id = SIWE_PROVIDER_CANISTER_ID.with_borrow(|maybe_id| *maybe_id);
+    let response = match maybe_id {
+        Some(siwe_provider_canister_id) => {
+            let response = IcSiweProvider(siwe_provider_canister_id)
+                .get_address(ByteBuf::from(caller.as_slice()))
+                .await
+                .map_err(|e| {
+                    Error::internal_server_error(format!("Code: {:?}, message: {}", e.0, e.1))
+                })?;
+
+            Ok(response)
+        }
+        None => Err(Error::internal_server_error(
+            "SIWE provider canister ID is not set.",
+        )),
+    }?;
 
     let address = match response.0 {
         GetAddressResponse::Ok(address) => address,
