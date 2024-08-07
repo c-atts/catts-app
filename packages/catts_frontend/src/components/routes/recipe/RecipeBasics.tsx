420001e93800d113b92f4f17c807d728df582e38
Kristofer <kristofer@fmckl.se>
2024-08-07 22:04:40 +0200

    RecipeBasics polishing

--- packages/catts_frontend/src/components/routes/recipe/RecipeBasics.tsx
+++ packages/catts_frontend/src/components/routes/recipe/RecipeBasics.tsx
@@ -2,6 +2,8 @@ import { formatDistance } from "date-fns";
 import { Badge } from "@/components/ui/badge";
 import useRecipeContext from "@/recipe/hooks/useRecipeContext";
 import UserLink from "@/components/UserLink";
+import Keywords from "./Keywords";
+import CopyButton from "@/components/CopyButton";
 
 export default function RecipeBasics() {
   const { recipe } = useRecipeContext();
@@ -11,6 +13,7 @@ export default function RecipeBasics() {
   }
 
   const {
+    id,
     name,
     display_name,
     description,
@@ -18,6 +21,8 @@ export default function RecipeBasics() {
     created,
     publish_state,
     keywords,
+    resolver,
+    revokable,
   } = recipe;
 
   const createdDate = new Date(created);
@@ -33,9 +38,41 @@ export default function RecipeBasics() {
       </div>
       <h1>{display_name || name}</h1>
       <div className="leading-relaxed">{description}</div>
-      <div className="text-sm text-foreground/50">Created {when}</div>
-      <div>Keywords</div>
-      <div>{keywords && keywords.join(", ")}</div>
+
+      <div className="flex flex-col w-full gap-2 text-sm">
+        <div className="flex w-full">
+          <div className="w-1/4 text-foreground/50 flex items-center">
+            Recipe Id:
+          </div>
+          <div className="w-3/4 flex items-center">
+            {id} <CopyButton className="ml-1" value={id} />
+          </div>
+        </div>
+        <div className="flex w-full">
+          <div className="w-1/4 text-foreground/50">Created:</div>
+          <div className="w-3/4">{when}</div>
+        </div>
+        <div className="flex w-full">
+          <div className="w-1/4 text-foreground/50">Keywords:</div>
+          <div className="w-3/4">
+            <Keywords keywords={keywords} />
+          </div>
+        </div>
+        {resolver !== "0x0000000000000000000000000000000000000000" && (
+          <div className="flex w-full">
+            <div className="w-1/4 text-foreground/50 flex items-center">
+              Resolver:
+            </div>
+            <div className="w-3/4 flex items-center">
+              {resolver} <CopyButton className="ml-1" value={resolver} />
+            </div>
+          </div>
+        )}
+        <div className="flex w-full">
+          <div className="w-1/4 text-foreground/50">Revokable:</div>
+          <div className="w-3/4">{revokable ? "Yes" : "No"}</div>
+        </div>
+      </div>
     </div>
   );
 }
