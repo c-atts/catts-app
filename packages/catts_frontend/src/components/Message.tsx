36976238ca5aa958a2f47e73c1d893e7d99695d7
Kristofer <kristofer@fmckl.se>
2024-08-07 17:29:49 +0200

    Add message component

---
+++ packages/catts_frontend/src/components/Message.tsx
@@ -0,0 +1,33 @@
+import { cn } from "@/lib/utils";
+
+type MessageType = "note" | "tip" | "important" | "warning" | "caution";
+
+export default function Message({
+  type,
+  children,
+  className,
+}: {
+  type: MessageType;
+  children: React.ReactNode;
+  className?: string;
+}) {
+  let colors = "";
+  switch (type) {
+    case "warning": {
+      colors = "bg-yellow-100/50 border-yellow-300";
+      break;
+    }
+    case "note": {
+      colors = "bg-blue-100/50 border-blue-300";
+      break;
+    }
+  }
+
+  console.log(className);
+
+  return (
+    <div className={cn(colors, "p-4 rounded-md border-[1px]", className)}>
+      {children}
+    </div>
+  );
+}
