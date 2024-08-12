94f8f0434522d5a6a18330488039865eba91e54d
Kristofer <kristofer@fmckl.se>
2024-08-12 10:55:49 +0200

    SchemaBadges component

---
+++ packages/catts_frontend/src/components/routes/run/SchemaBadges.tsx
@@ -0,0 +1,28 @@
+import { Badge } from "@/components/ui/badge";
+
+function SchemaBadge({ schemaItem }: { schemaItem: string }) {
+  const type = schemaItem.split(" ")[0];
+  const name = schemaItem.split(" ")[1];
+
+  return (
+    <Badge className="mr-1">
+      {name}: {type}
+    </Badge>
+  );
+}
+
+export default function SchemaBadges({ schema }: { schema?: string }) {
+  if (!schema) {
+    return null;
+  }
+
+  const schemaItems = schema.split(",");
+
+  return (
+    <div>
+      {schemaItems.map((schemaItem, index) => (
+        <SchemaBadge key={index} schemaItem={schemaItem} />
+      ))}
+    </div>
+  );
+}
