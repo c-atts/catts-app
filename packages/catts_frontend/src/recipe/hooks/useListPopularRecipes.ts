6d45ebbeb41f49b143af82886f93a12feb25865a
Kristofer <kristofer@fmckl.se>
2024-08-12 16:13:58 +0200

    Pagination for popular recipes

--- packages/catts_frontend/src/recipe/hooks/useListPopularRecipes.ts
+++ packages/catts_frontend/src/recipe/hooks/useListPopularRecipes.ts
@@ -1,12 +1,21 @@
 import { useSupabase } from "@/lib/supabase/SupabaseContext";
 import { useQuery } from "@tanstack/react-query";
 
-export const useListPopularRecipes = () => {
+export const useListPopularRecipes = ({
+  page,
+  limit,
+}: {
+  page: number;
+  limit: number;
+}) => {
   const supabase = useSupabase();
   return useQuery({
-    queryKey: ["recipe_popular_list"],
+    queryKey: ["recipe_popular_list", page, limit],
     queryFn: async () => {
-      const { data, error } = await supabase.rpc("list_popular_recipes");
+      const { data, error } = await supabase.rpc("list_popular_recipes", {
+        page,
+        pagesize: limit,
+      });
       if (error) throw error;
       return data;
     },
