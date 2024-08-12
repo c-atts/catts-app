baa7451d0305e3bbd580191aaeab80eb800d164b
Kristofer <kristofer@fmckl.se>
2024-08-12 15:10:48 +0200

    Pagination support for recipe listing

--- packages/catts_frontend/src/recipe/hooks/useListRecipes.ts
+++ packages/catts_frontend/src/recipe/hooks/useListRecipes.ts
@@ -1,15 +1,23 @@
 import { useSupabase } from "@/lib/supabase/SupabaseContext";
 import { useQuery } from "@tanstack/react-query";
 
-export const useListRecipes = () => {
+export const useListRecipes = ({
+  page,
+  limit,
+}: {
+  page: number;
+  limit: number;
+}) => {
   const supabase = useSupabase();
   return useQuery({
-    queryKey: ["recipe_list"],
+    queryKey: ["recipe_list", page, limit],
     queryFn: async () => {
       const { data, error } = await supabase
         .from("recipe")
         .select("id, name, description, creator, created")
-        .order("created", { ascending: false });
+        .order("created", { ascending: false })
+        .range((page - 1) * limit, page * limit - 1);
+
       if (error) throw error;
       return data;
     },
