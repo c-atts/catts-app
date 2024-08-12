d4e4b6e2bf340d3d01950f52825f1967bc9fef3b
Kristofer <kristofer@fmckl.se>
2024-08-12 12:41:22 +0200

    Support pagination for query

--- packages/catts_frontend/src/run/hooks/useListRuns.ts
+++ packages/catts_frontend/src/run/hooks/useListRuns.ts
@@ -1,14 +1,22 @@
 import { useSupabase } from "@/lib/supabase/SupabaseContext";
 import { useQuery } from "@tanstack/react-query";
 
-export const useListRuns = () => {
+export const useListRuns = ({
+  page,
+  limit,
+}: {
+  page: number;
+  limit: number;
+}) => {
   const supabase = useSupabase();
   return useQuery({
-    queryKey: ["runs_list"],
+    queryKey: ["runs_list", page, limit],
     queryFn: async () => {
       const { data, error } = await supabase
         .from("run")
-        .select(`id, created, creator, chain_id, recipe (name)`);
+        .select(`id, created, creator, chain_id, recipe (name)`)
+        .order("created", { ascending: false })
+        .range((page - 1) * limit, page * limit - 1);
       if (error) throw error;
       return data;
     },
