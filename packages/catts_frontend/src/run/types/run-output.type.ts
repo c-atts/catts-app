199326f72c0f77873ec03dfbbb455456a296993f
Kristofer <kristofer@fmckl.se>
2024-08-12 21:14:40 +0200

    Proper handling of uint256 in simulations

--- packages/catts_frontend/src/run/types/run-output.type.ts
+++ packages/catts_frontend/src/run/types/run-output.type.ts
@@ -1,15 +1,26 @@
+import { hexToBigInt } from "viem";
 import { z } from "zod";
 
-export const RunOutputDataItem = z.object({
-  name: z.string(),
-  type: z.string(),
-  value: z.union([
-    z.string(),
-    z.number(),
-    z.boolean(),
-    z.object({ hex: z.string() }),
-  ]),
-});
+export const RunOutputDataItem = z
+  .object({
+    name: z.string(),
+    type: z.string(),
+    value: z.union([
+      z.string(),
+      z.number(),
+      z.boolean(),
+      z.object({ hex: z.string() }),
+    ]),
+  })
+  .transform((item) => {
+    if (item.type === "uint256" && typeof item.value === "string") {
+      return {
+        ...item,
+        value: hexToBigInt(item.value as `0x${string}`).toString(),
+      };
+    }
+    return item;
+  });
 type RunOutputDataItem = z.infer<typeof RunOutputDataItem>;
 
 export const RunOutput = z.array(RunOutputDataItem);
