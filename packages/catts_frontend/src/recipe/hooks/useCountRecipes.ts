932c653b625e1466aad6f28f612e7a322770dc50
Kristofer <kristofer@fmckl.se>
2024-08-12 15:09:55 +0200

    Hook to count recipes

---
+++ packages/catts_frontend/src/recipe/hooks/useCountRecipes.ts
@@ -0,0 +1,16 @@
+import { useSupabase } from "@/lib/supabase/SupabaseContext";
+import { useQuery } from "@tanstack/react-query";
+
+export const useCountRecipes = () => {
+  const supabase = useSupabase();
+  return useQuery({
+    queryKey: ["recipes_count"],
+    queryFn: async () => {
+      const { count, error } = await supabase
+        .from("recipe")
+        .select("id", { count: "exact", head: true });
+      if (error) throw error;
+      return count;
+    },
+  });
+};
