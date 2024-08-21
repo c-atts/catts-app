319e7387187f87c2cb115032c714543a4086bf69
Kristofer <kristofer@fmckl.se>
2024-08-21 10:41:52 +0200

    Adjust styling

--- packages/catts_frontend/src/components/routes/recipe/RevealDetails.tsx
+++ packages/catts_frontend/src/components/routes/recipe/RevealDetails.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { ChevronDownIcon, ChevronUpIcon } from "lucide-react";
 import { useState } from "react";
 
 export default function RevealDetails({
@@ -9,13 +10,18 @@ export default function RevealDetails({
   const [isRevealed, setIsRevealed] = useState(false);
 
   return (
-    <div className="flex flex-col gap-5">
-      {isRevealed && children}
+    <div className="flex flex-col gap-5 mt-6">
       <div className="flex w-full justify-center">
         <Button onClick={() => setIsRevealed(!isRevealed)}>
           {isRevealed ? "Hide recipe details" : "Show recipe details"}
+          {isRevealed ? (
+            <ChevronUpIcon className="ml-2 w-4 h-4" />
+          ) : (
+            <ChevronDownIcon className="ml-2 w-4 h-4" />
+          )}
         </Button>
       </div>
+      {isRevealed && children}
     </div>
   );
 }
